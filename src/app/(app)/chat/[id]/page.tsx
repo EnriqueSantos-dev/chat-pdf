4a423a92060977b45a57e604407c9d6212c048b0
Enrique Santos <santosenrique869@gmail.com>
2024-03-05 09:11:56 -0300

    refactor: group app pages into group (app)

--- src/app/(app)/chat/[id]/page.tsx
+++ src/app/(app)/chat/[id]/page.tsx
@@ -1,15 +1,16 @@
+import { redirect } from "next/navigation";
+
+import { and, eq } from "drizzle-orm";
+import { currentUser } from "@clerk/nextjs";
+
 import { Chat } from "@/components/chat";
 import { ChatHistory } from "@/components/chat-history";
 import PDFViewer from "@/components/pdf-viewer";
+
 import { db } from "@/lib/db";
 import { chats as chatsTable } from "@/lib/db/schema";
-import { currentUser } from "@clerk/nextjs";
-import { and, eq } from "drizzle-orm";
-import { redirect } from "next/navigation";
 
-export const dynamic = "force-dynamic";
-
-export default async function page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
   const chats = await db.query.chats.findMany({
     with: {
@@ -17,8 +18,6 @@ export default async function page({ params }: { params: { id: string } }) {
     },
     where: and(eq(chatsTable.userId, user?.id!), eq(chatsTable.id, params.id)),
   });
-
-  console.log("chats", chats);
 
   if (chats.length === 0) {
     return redirect("/");
