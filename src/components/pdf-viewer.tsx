e1396ba17a2fb4878d2c3c455054db9e7907b501
Enrique Santos <santosenrique869@gmail.com>
2024-03-18 11:26:29 -0300

    chore: add PDF viewer component and dependencies

--- src/components/pdf-viewer.tsx
+++ src/components/pdf-viewer.tsx
@@ -1,12 +1,21 @@
+"use client";
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+
 type PDFViewerProps = {
   pdfURL: string;
 };
 
 export default function PDFViewer({ pdfURL }: PDFViewerProps) {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin({});
+
   return (
-    <iframe
-      src={`https://docs.google.com/gview?url=${pdfURL}&embedded=true`}
-      className="size-full scroll-smooth"
-    />
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+      <div className="max-h-full overflow-auto">
+        <Viewer fileUrl={pdfURL} plugins={[defaultLayoutPluginInstance]} />
+      </div>
+    </Worker>
   );
 }
